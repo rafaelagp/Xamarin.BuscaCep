12dc41c25adcb68e79a6e68bdb2e1ca1bf0f263e
Rafael Pereira <rafaeel.agp@globo.com>
2016-05-25 16:45:37 -0300

    Estrutura inicial do app.

---
+++ BuscaCepApp/BuscaCepApp/Model/Endereco.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+
+namespace BuscaCepApp.Model
+{
+    public class Endereco : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string _cep;
+        public string Cep
+        {
+            get { return _cep; }
+            set
+            {
+                _cep = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(_cep)));   
+            }
+        }
+
+        private string _logradouro;
+        public string Logradouro
+        {
+            get { return _logradouro; }
+            set
+            {
+                _logradouro = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(_logradouro)));
+            }
+        }
+
+        private string _bairro;
+        public string Bairro
+        {
+            get { return _bairro; }
+            set
+            {
+                _bairro = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(_bairro)));
+            }
+        }
+
+        private string _localidade;
+        public string Localidade
+        {
+            get { return _localidade; }
+            set
+            {
+                _localidade = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(_localidade)));
+            }
+        }
+
+        private string _uf;
+        public string Uf
+        {
+            get { return _logradouro; }
+            set
+            {
+                _uf = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(_uf)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ BuscaCepApp/BuscaCepApp/ViewModel/EnderecoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using BuscaCepApp.Model;
+
+namespace BuscaCepApp.ViewModel
+{
+    public class EnderecoViewModel : INotifyPropertyChanged
+    {
+        public ICommand BuscarCommand { get; set; }
+
+        private IObserver<Endereco> _endereco;
+        public IObserver<Endereco> Endereco
+        {
+            get { return _endereco; }
+            set
+            {
+                _endereco = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Endereco)));
+            }
+        }
+
+        public EnderecoViewModel()
+        {
+            BuscarCommand = new Xamarin.Forms.Command(async () =>
+            {
+                
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
